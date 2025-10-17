a26fa96375f4578970d2819888608ae39fa8ff28
maksymaf <forbidden344@gmail.com>
2025-10-17 17:47:19 +0300

    chore: initial project setup with express and mongo

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({origin: ['http://localhost:3000', 'http://localhost:5500']}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+(async function main() {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+        app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+    }catch(error){
+        console.error(`Failed to connect to MongoDB ${error.stack}`);
+        process.exit(1);
+    }
+})();
