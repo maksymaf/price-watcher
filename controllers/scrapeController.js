9331d839ba4c3cac3e3358dd2572210ebeb778d2
maksymaf <forbidden344@gmail.com>
2025-10-24 18:42:01 +0300

    refactor: Add service folder with scrape logic

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -1,24 +1,12 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const { scrape } = require('../services/scrapeService');
 
 const ScrapeController = {
     async scrapeProduct(req, res) {
         try{
             const { url } = req.body;
-            const { data } = await axios.get(url, {
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                    'Accept-Language': 'uk-UA,uk;q=0.9,en;q=0.8',
-                }
-            });
-
-            const $ = cheerio.load(data);
-
-            const name = $('[data-qaid="image_preview"]').attr('alt').split('-')[0].trim();
-            const image = $('[data-qaid="image_preview"]').attr('src');
-            const price = parseInt($('[data-qaid="main_product_info"] [data-qaid="product_price"]').text().split(/\s+/)[0]);
+            const data = await scrape(url);
             
-            res.status(200).json({name, image, price, url});
+            res.status(200).json(data);
         }catch(error){
             console.error(error.message);
             res.status(500).json({message: error.message});
---
+++ services/scrapeService.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function scrape(url) {
+    const { data } = await axios.get(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept-Language': 'uk-UA,uk;q=0.9,en;q=0.8',
+        }
+    });
+
+    const $ = cheerio.load(data);
+
+    const name = $('[data-qaid="image_preview"]').attr('alt').split('-')[0].trim();
+    const image = $('[data-qaid="image_preview"]').attr('src');
+    const price = parseInt($('[data-qaid="main_product_info"] [data-qaid="product_price"]').text().split(/\s+/)[0]);
+
+    return {name, image, price, url}
+}
+
+module.exports = {scrape};
